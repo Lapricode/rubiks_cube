1dc696e592431d3aeb14e08d5629322d0be59f2b
Printzios Lampros <91993549+Lapricode@users.noreply.github.com>
2022-08-25 10:24:44 +0300

    Add files via upload

--- rubiks_cube.py
+++ rubiks_cube.py
@@ -7,23 +7,30 @@ class rubiks_cube():
         self.root = root
         self.root.title("Rubik's cube")
         self.root.geometry("+0+0")
-        self.cube_background_width = (4 / 5) * self.root.winfo_screenheight()
+        self.cube_background_width = (3 / 4) * self.root.winfo_screenheight()
         self.cube_background_height = self.cube_background_width
+        self.play_background_width = self.cube_background_width
+        self.play_background_height = self.cube_background_height / 6
         self.menu_background_width = self.cube_background_width / 2
-        self.menu_background_height = self.cube_background_height
+        self.menu_background_height = self.cube_background_height + self.play_background_height
         self.menu_background = tk.Frame(root, width = self.menu_background_width, height = self.menu_background_height, bg = "black", bd = 0, relief = "solid")
-        self.menu_background.grid(row = 0, column = 0, sticky = tk.NSEW)
+        self.menu_background.grid(row = 0, column = 0, rowspan = 2, sticky = tk.NSEW)
         self.cube_background = tk.Canvas(root, width = self.cube_background_width, height = self.cube_background_height, bg = "cyan", bd = 0, relief = "solid")
         self.cube_background.grid(row = 0, column = 1, sticky = tk.NSEW)
-
+        self.play_background = tk.Canvas(root, width = self.play_background_width, height = self.play_background_height, bg = "yellow", bd = 0, relief = "solid")
+        self.play_background.grid(row = 1, column = 1, sticky = tk.NSEW)
         self.square_side_length = 100
+        self.square_side_lengths_matrix = [25, 50, 75, 100, 125]
         self.borders_width = 5
+        self.borders_widths_matrix = [0, 2, 5, 10, 20]
         self.sides_distortion = 0.4
+        self.sides_distortions_matrix = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
         centre_offset =  3 / 2 * self.sides_distortion * self.square_side_length
         self.front_cube_side_centre_point = [self.cube_background_width / 2 - centre_offset / math.sqrt(2), self.cube_background_height / 2 + centre_offset / math.sqrt(2)]
         self.moves_speed = 0
-        self.scramble_moves_number = 10
-        self.rubiks_cube = [[[["b", "y", "o", "w", "r", "g"][x] for z in range(3)] for y in range(3)] for x in range(6)]
+        self.moves_speeds_matrix = [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0]
+        self.scramble_moves = 10
+        self.scramble_moves_matrix = [1, 3, 5, 10, 20]
         self.reset_cube()
         # scramble_cube(rubiks_cube, 20)
 
@@ -36,7 +43,7 @@ class rubiks_cube():
         self.moves_speed_button = menu_button(self.menu_background, self.moves_speed, "Arial 20 bold", "white", "black", scramble_cube_menu_cors[0] + 80, scramble_cube_menu_cors[1] + 60, self.change_game_settings).button
         self.moves_number_label = menu_label(self.menu_background, "moves\nnumber", "Arial 18 bold", "red", "black", scramble_cube_menu_cors[0] - 30, scramble_cube_menu_cors[1] + 120).label
         self.moves_number_label2 = menu_label(self.menu_background, ":", "Arial 18 bold", "red", "black", scramble_cube_menu_cors[0] + 50, scramble_cube_menu_cors[1] + 120).label
-        self.moves_number_button = menu_button(self.menu_background, self.scramble_moves_number, "Arial 20 bold", "white", "black", scramble_cube_menu_cors[0] + 80, scramble_cube_menu_cors[1] + 120, self.change_game_settings).button
+        self.moves_number_button = menu_button(self.menu_background, self.scramble_moves, "Arial 20 bold", "white", "black", scramble_cube_menu_cors[0] + 80, scramble_cube_menu_cors[1] + 120, self.change_game_settings).button
         self.scramble_button = menu_button(self.menu_background, "scramble", "Arial 25 bold", "white", "black", scramble_cube_menu_cors[0], scramble_cube_menu_cors[1] + 180, self.scramble_cube_event).button
 
         cube_graphics_menu_cors = [self.menu_background_width / 2, 380]
@@ -52,34 +59,34 @@ class rubiks_cube():
         self.borders_width_button = menu_button(self.menu_background, self.borders_width, "Arial 20 bold", "white", "black", cube_graphics_menu_cors[0] + 80, cube_graphics_menu_cors[1] + 180, self.change_game_settings).button
 
         self.bindings_are_activated = True
-        self.root.bind('<Left>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<Right>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<Up>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<Down>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<x>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<X>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<y>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<Y>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<z>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<Z>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<r>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<R>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<l>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<L>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<u>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<U>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<d>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<D>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<f>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<F>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<b>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<B>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<m>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<M>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<e>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<E>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<s>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
-        self.root.bind('<S>', lambda event: self.make_move_graphic(event, [], self.rubiks_cube))
+        self.root.bind('<Left>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<Right>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<Up>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<Down>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<x>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<X>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<y>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<Y>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<z>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<Z>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<r>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<R>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<l>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<L>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<u>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<U>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<d>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<D>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<f>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<F>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<b>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<B>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<m>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<M>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<e>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<E>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<s>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
+        self.root.bind('<S>', lambda event: self.make_move_graphic(event, self.rubiks_cube, []))
 
     def left(self, cube):
         cube2 = [[[cube[x][y][z] for z in range(3)] for y in range(3)] for x in range(6)]
@@ -267,8 +274,8 @@ class rubiks_cube():
 
     def reset_cube(self):
         self.rubiks_cube = [[[["b", "y", "o", "w", "r", "g"][x] for z in range(3)] for y in range(3)] for x in range(6)]
-        self.moves_seq_copy = []
-        self.make_move_graphic(None, [], self.rubiks_cube)
+        self.play_background.delete("all")
+        self.make_move_graphic(None, self.rubiks_cube, [])
     def reset_cube_event(self, event):
         if self.bindings_are_activated: self.reset_cube()
     def scramble_cube(self, cube, moves_number):
@@ -277,30 +284,31 @@ class rubiks_cube():
         moves_matrix = ["L", "L'", "R", "R'", "U", "U'", "D", "D'", "F", "F'", "B", "B'", "M", "M'", "E", "E'", "S", "S'"]
         for i in range(moves_number):
             self.moves_seq.append(random.choice(moves_matrix))
-        self.moves_seq_copy = [x for x in self.moves_seq]
-        self.make_move_graphic(None, [""] + self.moves_seq, cube)
+        self.play_background.delete("all")
+        self.moves_seq_text = self.play_background.create_text(float(self.play_background["width"]) / 2, 30, fill = "darkblue", text = self.moves_seq, font = "Times 20 italic bold")
+        self.make_move_graphic(None, cube, [""] + self.moves_seq)
     def scramble_cube_event(self, event):
         if self.bindings_are_activated:
-            self.scramble_cube(self.rubiks_cube, self.scramble_moves_number)
+            self.scramble_cube(self.rubiks_cube, self.scramble_moves)
     def change_game_settings(self, event):
         if event.widget == self.moves_speed_button:
-            self.moves_speed = self.alternate_matrix_elements([0, 0.1, 0.5, 1.0, 2.0, 5.0], self.moves_speed)
+            self.moves_speed = self.alternate_matrix_elements(self.moves_speeds_matrix, self.moves_speed)
             self.moves_speed_button.configure(text = self.moves_speed)
         elif event.widget == self.moves_number_button:
-            self.scramble_moves_number = self.alternate_matrix_elements([1, 3, 5, 10, 20], self.scramble_moves_number)
-            self.moves_number_button.configure(text = self.scramble_moves_number)
+            self.scramble_moves = self.alternate_matrix_elements(self.scramble_moves_matrix, self.scramble_moves)
+            self.moves_number_button.configure(text = self.scramble_moves)
         elif event.widget == self.cube_size_button:
-            self.square_side_length = self.alternate_matrix_elements([25, 50, 75, 100, 125], self.square_side_length)
+            self.square_side_length = self.alternate_matrix_elements(self.square_side_lengths_matrix, self.square_side_length)
             self.cube_size_button.configure(text = self.square_side_length)
         elif event.widget == self.sides_distortion_button:
-            self.sides_distortion = self.alternate_matrix_elements([0.0, 0.2, 0.4, 0.6, 0.8, 1.0], self.sides_distortion)
+            self.sides_distortion = self.alternate_matrix_elements(self.sides_distortions_matrix, self.sides_distortion)
             self.sides_distortion_button.configure(text = self.sides_distortion)
         elif event.widget == self.borders_width_button:
-            self.borders_width = self.alternate_matrix_elements([0, 2, 5, 10, 20], self.borders_width)
+            self.borders_width = self.alternate_matrix_elements(self.borders_widths_matrix, self.borders_width)
             self.borders_width_button.configure(text = self.borders_width)
         centre_offset =  3 / 2 * self.sides_distortion * self.square_side_length
         self.front_cube_side_centre_point = [float(self.cube_background["width"]) / 2 - centre_offset / math.sqrt(2), float(self.cube_background["height"]) / 2 + centre_offset / math.sqrt(2)]
-        self.make_move_graphic(None, [], self.rubiks_cube)
+        self.make_move_graphic(None, self.rubiks_cube, [])
     def alternate_matrix_elements(self, matrix, index_element):
         return (matrix[1:] + [matrix[0]])[matrix.index(index_element)]
 
@@ -316,30 +324,26 @@ class rubiks_cube():
         cfdl2 = [cfdl[0] + 2 * ps1 * math.cos(pa1), cfdl[1] - 2 * ps1 * math.sin(pa1)]; background.create_polygon([cfdl2[0], cfdl2[1], cfdl2[0] + ps2 * math.cos(pa2), cfdl2[1] - ps2 * math.sin(pa2), cfdl2[0] + ps2 * math.cos(pa2) + ps1 * math.cos(pa1), cfdl2[1] - ps1 * math.sin(pa1) - ps2 * math.sin(pa2), cfdl2[0] + ps1 * math.cos(pa1), cfdl2[1] - ps1 * math.sin(pa1)], fill = cf_colors[0][0], width = borders_width, outline = "black", tags = "up_left_piece")
         cfdl2 = [cfdl[0] + 2 * ps1 * math.cos(pa1) + ps2 * math.cos(pa2), cfdl[1] - 2 * ps1 * math.sin(pa1) - ps2 * math.sin(pa2)]; background.create_polygon([cfdl2[0], cfdl2[1], cfdl2[0] + ps2 * math.cos(pa2), cfdl2[1] - ps2 * math.sin(pa2), cfdl2[0] + ps2 * math.cos(pa2) + ps1 * math.cos(pa1), cfdl2[1] - ps1 * math.sin(pa1) - ps2 * math.sin(pa2), cfdl2[0] + ps1 * math.cos(pa1), cfdl2[1] - ps1 * math.sin(pa1)], fill = cf_colors[0][1], width = borders_width, outline = "black", tags = "up_middle_piece")
         cfdl2 = [cfdl[0] + 2 * ps1 * math.cos(pa1) + 2 * ps2 * math.cos(pa2), cfdl[1] - 2 * ps1 * math.sin(pa1) - 2 * ps2 * math.sin(pa2)]; background.create_polygon([cfdl2[0], cfdl2[1], cfdl2[0] + ps2 * math.cos(pa2), cfdl2[1] - ps2 * math.sin(pa2), cfdl2[0] + ps2 * math.cos(pa2) + ps1 * math.cos(pa1), cfdl2[1] - ps1 * math.sin(pa1) - ps2 * math.sin(pa2), cfdl2[0] + ps1 * math.cos(pa1), cfdl2[1] - ps1 * math.sin(pa1)], fill = cf_colors[0][2], width = borders_width, outline = "black", tags = "up_right_piece")
-    def make_move_graphic(self, event, moves_seq, cube):
+    def make_move_graphic(self, event, cube, moves_seq):
         if event == None:
+            self.cube_background.delete("all")
             self.bindings_are_activated = False
             if moves_seq == []:
-                self.cube_background.delete("all")
-                self.moves_seq_text = self.cube_background.create_text(float(self.cube_background["width"]) / 2, 30, fill = "darkblue", text = self.moves_seq_copy, font = "Times 20 italic bold")
                 # in order: front_cube_side, up_cube_side, right_cube_side
                 self.draw_cube_side(self.cube_background, self.rubiks_cube[5], self.borders_width, self.square_side_length, self.square_side_length, 90, 0, [self.front_cube_side_centre_point[0] - 3 * self.square_side_length / 2, self.front_cube_side_centre_point[1] + 3 * self.square_side_length / 2])
                 self.draw_cube_side(self.cube_background, self.rubiks_cube[3], self.borders_width, self.square_side_length * self.sides_distortion, self.square_side_length, 45, 0, [self.front_cube_side_centre_point[0] - 3 * self.square_side_length / 2, self.front_cube_side_centre_point[1] - 3 * self.square_side_length / 2])
                 self.draw_cube_side(self.cube_background, [[self.rubiks_cube[4][2-j][i] for j in range(3)] for i in range(3)], self.borders_width, self.square_side_length, self.square_side_length * self.sides_distortion, 90, 45, [self.front_cube_side_centre_point[0] + 3 * self.square_side_length / 2, self.front_cube_side_centre_point[1] + 3 * self.square_side_length / 2])
                 self.bindings_are_activated = True
             else:
-                self.cube_background.delete("all")
-                self.moves_seq_text = self.cube_background.create_text(float(self.cube_background["width"]) / 2, 30, fill = "darkblue", text = self.moves_seq_copy, font = "Times 20 italic bold")
                 move = []
                 move.append(moves_seq[0])
                 self.rubiks_cube = self.make_moves_sequence(cube, move)
                 self.draw_cube_side(self.cube_background, self.rubiks_cube[5], self.borders_width, self.square_side_length, self.square_side_length, 90, 0, [self.front_cube_side_centre_point[0] - 3 * self.square_side_length / 2, self.front_cube_side_centre_point[1] + 3 * self.square_side_length / 2])
                 self.draw_cube_side(self.cube_background, self.rubiks_cube[3], self.borders_width, self.square_side_length * self.sides_distortion, self.square_side_length, 45, 0, [self.front_cube_side_centre_point[0] - 3 * self.square_side_length / 2, self.front_cube_side_centre_point[1] - 3 * self.square_side_length / 2])
                 self.draw_cube_side(self.cube_background, [[self.rubiks_cube[4][2-j][i] for j in range(3)] for i in range(3)], self.borders_width, self.square_side_length, self.square_side_length * self.sides_distortion, 90, 45, [self.front_cube_side_centre_point[0] + 3 * self.square_side_length / 2, self.front_cube_side_centre_point[1] + 3 * self.square_side_length / 2])
-                self.cube_background.after(int(1000 * self.moves_speed), lambda: self.make_move_graphic(None, moves_seq[1:], self.rubiks_cube))
+                self.cube_background.after(int(1000 * self.moves_speed), lambda: self.make_move_graphic(None, self.rubiks_cube, moves_seq[1:]))
         elif event != None and self.bindings_are_activated:
             self.cube_background.delete("all")
-            self.moves_seq_text = self.cube_background.create_text(float(self.cube_background["width"]) / 2, 30, fill = "darkblue", text = self.moves_seq_copy, font = "Times 20 italic bold")
             if event.keysym == "Left":
                 self.rubiks_cube = self.make_moves_sequence(cube, ["y"])
             elif event.keysym == "Right":
